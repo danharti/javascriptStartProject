b7790093cc70c113625eda5e9ca6bd286407b307
D <d@chie.com>
2015-09-02 04:38:38 +0800

    addedPrompt

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
-alert("Hello");
-document.write("<h1>Hello</h1>");
-alert("Thanks");
+//activates the prompt box - add your text inside
+//by adding it in a variable, we are capturing the data that was inputed by the user
+var address = prompt("Where are you right now?");
+alert("Are you sure? You are at " + address);
+
+document.write("<h1>Welcome to " + address + "</h1>");
