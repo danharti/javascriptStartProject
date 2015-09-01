d1e9d6bdd7e1915e6e6a6d83a30c90c9048a41ce
D <d@chie.com>
2015-09-02 03:32:38 +0800

    first branch change

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-alert("Hello");
-document.write("<h1>Hello</h1>");
-alert("Thanks");
+
+//javascript statement (it always end with semicolons)
+// function alert to pass string "hello"
+alert("Hello, this is my first branch at github");
+
+//function to write to document and pass string hello in h1 size
+document.write("<h1>My First Branch!!!</h1>");
