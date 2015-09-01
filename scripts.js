6b6aac693299d634bdbbc4e8df048a15487cc52a
D <d@chie.com>
2015-09-02 04:18:37 +0800

    added a variable

--- scripts.js
+++ scripts.js
@@ -1,7 +1,23 @@
 
 //javascript statement (it always end with semicolons)
 // function alert to pass string "hello"
+
+var message = "first branch"
+alert(message);
 alert("Hello, this is my first branch at github");
 
 //function to write to document and pass string hello in h1 size
 document.write("<h1>My First Branch!!!</h1>");
+
+//variables
+
+//create a variable and name it
+var box;
+
+//assigning a value to a variable
+var boxes = 0;
+boxes = 1;
+//no need to write the word var again to assign a new value.
+//concept here is that you create the variable only once, and then assign the value necessarily.
+
+
