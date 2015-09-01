bc52675deb672173d0b3df59fc2964e97508dc4b
D <d@chie.com>
2015-09-02 04:41:09 +0800

    change some text

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //activates the prompt box - add your text inside
 //by adding it in a variable, we are capturing the data that was inputed by the user
 var address = prompt("Where are you right now?");
-alert("Are you sure? You are at " + address);
+alert("Are you sure you are at " + address + "?");
 
 document.write("<h1>Welcome to " + address + "</h1>");
