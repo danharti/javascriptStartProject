73b475e2e25b1db79fff58d972b94f5abcfea782
D <d@chie.com>
2015-09-02 13:20:37 +0800

    added true or false

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
+var correctGuess = false;
+//this was set to false since the person hasn't guess any number yet
+
 var yourGuess = parseInt (prompt ("What is your guess?"));
 var randomNumber = Math.floor ( Math.random() * 10 ) + 1;
 
 if (yourGuess === randomNumber){
+	correctGuess = true;
+}
+
+if (correctGuess){
 	document.write("<h2>You got it right! Nice Guess</h2>");
 } else {
 	document.write("<h2>Try again. The lucky number is " + randomNumber + ". </h2>");
 }
 
+//Booleans true or false
