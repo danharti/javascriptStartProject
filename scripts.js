718ae122ee7db8129365fa51155b4a8304db034d
D <d@chie.com>
2015-09-02 08:46:07 +0800

    some basic text changes only

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
-alert("Hello");
-document.write("<h1>Hello</h1>");
-alert("Thanks");
+
+var myObject = prompt("What is your object?")
+var methodYou = prompt("What do you want me to do?");
+
+var propertyAndObject = myObject + "'." + methodYou + "();";
+
+var methodCallOnObject = myObject[methodYou]();
+console.log(methodCallOnObject);
+document.write("<h1>" + methodCallOnObject + "</h1>");
+
+var message = "For now it is just some Methods such as toLowerCase, toUpperCase those without any arguments or parmeters being passed. I'll think of how I can passed the parameters next. hmmmm."
+// var message = myObject.length;
+
+document.write("<h2> '" + myObject + "' is your object. <br><br> If you want to call the method, " + methodYou +" in your object '" + myObject + "'. <br><br> TYPE: <br><br>'" + propertyAndObject + "<br><br> The RESULT would be <br><br>" + methodCallOnObject + "<br> <br> Future explanation on the method to be added here.<h2> <br>" + message);
