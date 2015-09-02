40a6d49babb62d10ec4a5da4ef6c67d4be4a5743
D <d@chie.com>
2015-09-02 13:06:38 +0800

    one prompt with a question, one answer, two different responses.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-alert("Hello");
-document.write("<h1>Hello</h1>");
-alert("Thanks");
+var answer = prompt("What is the greenbox in my room?");
+
+//no semicolons on the end of if statement
+//=== equality operator
+if (answer.toUpperCase() === "CREEPER" ) {
+	document.write("<p> Your correct! </p>");
+} else {
+	document.write("<p> Your wrong! It's a creeper </p>");
+}
+
+//conditional statements lets you control the flow of a program (which code runs and when.)
+//else - if not true
+
+//computer thinks - true or false
+
