d4a68b8b02835f58a49be1ffb9da6a024e18af43
D <d@chie.com>
2015-09-02 09:44:10 +0800

    finalize the challenge of how many days alive

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 //test changes
+var askAge = prompt("What is your age?");
+
+//variables to count time 
+var secondsPerMinute = 60;
+var minutesPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+
+var agePerSecond = askAge * secondsPerMinute * minutesPerHour * hoursPerDay * daysPerWeek * weeksPerYear;
+
+document.write("<h1>" + agePerSecond + "seconds alive!!! </h1>");
