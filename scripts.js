110501d5ccf744d48ef96c8c86b1d510154771f8
D <d@chie.com>
2015-09-03 12:07:22 +0800

    added conditions if you have money and it's a Friday

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
-alert("Hello");
-document.write("<h1>Hello</h1>");
-alert("Thanks");
+var money = parseInt(prompt("How much money do you have today?"));
+var today = prompt("What day is it today?");
+
+//next, no need to ask the day, just compute it.
+
+if ( money >= 100 && today.toLowerCase() === 'friday' ) {
+  document.write("<h2>Time to treat a friend to the theater.<h2>");    
+} else if ( money >= 50 && today.toLowerCase() === 'friday' ) {
+  document.write("<h2>Time for a movie and dinner</h2>");    
+} else if ( money > 10 && today.toLowerCase() === 'friday' ) {
+  document.write("<h2>Time for a movie</h2>");   
+} else if ( today.toLowerCase() !== 'friday' ) {
+  document.write("<h2>It's Friday, but I don't have enough money to go out</h2>");   
+} else {
+  document.write("<h2>This isn't Friday. I need to stay home.<h2>");
+}
