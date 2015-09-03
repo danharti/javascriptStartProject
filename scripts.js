d0a4dfd967d20cdc0a9ef574fe79e92b2fa69138
D <d@chie.com>
2015-09-03 13:54:22 +0800

    functions different kinds

--- scripts.js
+++ scripts.js
@@ -1,3 +1,42 @@
-alert("Hello");
-document.write("<h1>Hello</h1>");
-alert("Thanks");
+function functionName(){
+	//code here
+}
+
+var functionPassingAnAnonymousFunction = function () {
+	//code here 
+	//take note it has semicolons
+};
+
+//function that returns a value....
+//return value from a function - use return
+//return statement exits the function immediately
+
+function randomNumber(){
+	var randomNumero = Math.floor (Math.random() * 10) + 1;
+	return randomNumero;
+}
+
+//very useful when returning a value and using it in different scenarios
+alert(randomNumber());
+document.write(randomNumber());
+var randomNumero10 = randomNumber();
+
+
+
+
+
+//To test if email field has content
+
+// function isEmailFieldEmpty() {
+// 	var field = document.getElementById('email');
+// 	if (field.value === ''){
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+
+// var fieldTest = isEmailFieldEmpty();
+// if (fieldTest === true) {
+// 	alert("Please provide your email. Thanks.");
+// }
