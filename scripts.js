cba57d55eaf9ba4dd2a1e64ae255e592728e3457
D <d@chie.com>
2015-09-03 11:56:48 +0800

    added more question to the quiz and added a point system

--- scripts.js
+++ scripts.js
@@ -1,12 +1,81 @@
-var answer = prompt("What is the greenbox in my room?");
+var question1 = "What is the greenbox in my room?";
+var question2 = "What is the greenbox in my room?";
+var question3 = "What is the greenbox in my room?";
+var question4 = "What is the greenbox in my room?";
+var question5 = "What is the greenbox in my room?";
+
+var answer1 = prompt(question1);
+var answer2 = prompt(question2);
+var answer3 = prompt(question3);
+var answer4 = prompt(question4);
+var answer5 = prompt(question5);
+
+var correctAnswer1 = "creeper";
+var correctAnswer2 = "creeper";
+var correctAnswer3 = "creeper";
+var correctAnswer4 = "creeper";
+var correctAnswer5 = "creeper";
+
+var points = 0;
+var message = "<br>";
+var ranking = " ";
 
 //no semicolons on the end of if statement
 //=== equality operator
-if (answer.toUpperCase() === "CREEPER" ) {
-	document.write("<p> Your correct! </p>");
+if (answer1.toLowerCase() === correctAnswer1 ) {
+	points += 1;
+	message += "#1 " + answer1 + " is correct <br>";
 } else {
-	document.write("<p> Your wrong! It's a creeper </p>");
+	message += "#1 " + answer1 + " is wrong, it should be " + correctAnswer1 + " <br>";
 }
+
+if (answer2.toLowerCase() === correctAnswer2 ) {
+	points += 1;
+	message += "#2 " + answer2 + " is correct <br>";
+} else {
+	message += "#2 " + answer2 + " is wrong, it should be " + correctAnswer2 + " <br>";
+}
+ 
+
+if (answer3.toLowerCase() === correctAnswer3 ) {
+	points += 1;
+	message += "#3 " + answer3 + " is correct <br>";
+} else {
+	message += "#3 " + answer3 + " is wrong, it should be " + correctAnswer3 + " <br>";
+}
+
+if (answer4.toLowerCase() === correctAnswer4 ) {
+	points += 1;
+	message += "#4 " + answer4 + " is correct <br>";
+} else {
+	message += "#4 " + answer4 + " is wrong, it should be " + correctAnswer4 + " <br>";
+}
+
+if (answer5.toLowerCase() === correctAnswer5 ) {
+	points += 1;
+	message += "#4 " + answer5 + " is correct <br>";
+} else {
+	message += "#4 " + answer5 + " is wrong, it should be " + correctAnswer5 + " <br>";
+}
+
+if (points === 5){
+	ranking = "You got a GOLD CROWN";
+} 
+//i'm using the && method here just to test it. We can also do points >= 3
+else if (points < 5 && points > 2){
+	ranking = "You got a SILVER CROWN";
+} else {
+	ranking = "You must study harder - You got no crown.";
+}
+
+document.write("<h2>" + points + "<br>" + message + "<br><br>" + ranking + "</h2>");
+
+
+// if (answer1.toLowerCase() === correctAnswer1 ) {
+// 	document.write("<p> Your correct! </p>");
+// } else {
+// 	document.write("<p> Your wrong! It's a creeper </p>");
+// }
 
 //conditional statements lets you control the flow of a program (which code runs and when.)
 //else - if not true
