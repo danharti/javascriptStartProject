30e25849e87a0b59825eaacdcaff4209df5c8a45
D <d@chie.com>
2015-09-04 09:22:40 +0800

    for forLoops

--- scripts.js
+++ scripts.js
@@ -1,29 +1,9 @@
-var randomNumber = randomNumber(10);
-var guess;
-var guessCount = 0;
-var correctGuess = false;
-//FLAG - a variable that holds a true or false value
+var html = "";
 
-function randomNumber(upper){
-	//random number starting from 1
-	return Math.floor(Math.random() * upper) + 1;
+//the div here fits until 5 digits
+for (var i = 100; i <= 2000; i += 100){
+	html += "<div>" + i + "</div>";
 }
 
-// do {
-// 	guess = parseInt(prompt("What is your guess?"));
-// 	guessCount += 1;
-// } while (guess != randomNumber)
-
-do {
-	guess = parseInt(prompt("What is your guess?"));
-	guessCount += 1;
-	if (guess === randomNumber){
-		correctGuess = true;
-	}
-} while (!correctGuess)
-
-//if true do while loop... if false it stops
-
-document.write (randomNumber + " is the randomNumber. You have guessed it " + guessCount + " times.")
-
+document.write(html);
 
